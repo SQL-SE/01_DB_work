dda77d6f49ed601ba8840708a8e062cbab8b0946
sirikid1710 <seit201@aol.com>
2022-12-07 09:02:42 +0000

    updatet from god :-P

---
+++ 03_user_DB/03_db_userDB_SQL.sql
@@ -0,0 +1,25 @@
+/* USER DB Vers. 1 */
+
+
+/* DBs */
+DROP DATABASE IF EXISTS boo;
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* Table users */
+DROP TABLE IF EXISTS boo.users;
+CREATE TABLE IF NOT EXISTS boo.users(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(20) NOT NULL,
+    familyName VARCHAR(20) NOT NULL,
+    firstName VARCHAR(20) NOT NULL
+);
+
+DESCRIBE boo.users;
+
+/* Daten */
+INSERT INTO boo.users(firstName,familyName,userName) VALUES ("Max","Mütze","max");
+INSERT INTO boo.users(firstName,familyName,userName) VALUES ("Maxine","Mütze","maxine");
+INSERT INTO boo.users(firstName,familyName,userName) VALUES ("Max","Mützerich","max")
+
+/* Inhalte : Ergebnistabelle */
+SELECT * FROM boo.users;
