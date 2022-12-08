29e880f841f8ff1f62aebe0aabb9505e01591290
sirikid1710 <seit201@aol.com>
2022-12-08 11:30:46 +0000

    05_4

--- 05_SQL_SELECT/05_3_que_sort.sql
+++ 05_SQL_SELECT/05_3_que_sort.sql
@@ -11,14 +11,14 @@ SELECT
 FROM stocks.ccc
 -- Sortierung
 #ORDER BY ticker ASC -- SYM, alphabetisch, aufsteigend
-ORDER BY ticker DESC -- SYM, alphabetisch, absteigend
+#ORDER BY ticker DESC -- SYM, alphabetisch, absteigend
 
 #ORDER BY price ASC -- Kurs, numerisch, beginnend mit der günstigsten Aktie
 #ORDER BY price DESC -- Kurs, numerisch, beginnend mit der teuerste Aktie
 #ORDER BY no_yrs DESC -- numerisch, Aktien mit der längsten Div.-Historie
 
 #ORDER BY sector DESC,industry ASC  -- Kombination ASC / DESC alphabet.
-#ORDER BY no_yrs DESC, sector ASC  -- Kombination ASC / DESC  alphabet. / numerisch
+ORDER BY no_yrs DESC, sector ASC  -- Kombination ASC / DESC  alphabet. / numerisch
 
 -- Begrenzung
 LIMIT 40
